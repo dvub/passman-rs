c29721c5e419f7f467300c2056bcebe7cacf9d4c
kaya <84478450+dvub@users.noreply.github.com>
2023-10-12 18:42:22 -0700

    work on basic functions

--- src/db_ops.rs
+++ src/db_ops.rs
@@ -1,19 +1,20 @@
-use rusqlite::{params, Connection};
+use rusqlite::{Connection, OptionalExtension};
 
 #[derive(Debug)]
-struct Password {
+pub struct Password {
     id: i32,
     name: String,
-    email: Option<Vec<u8>>,
-    username: Option<Vec<u8>>,
-    notes: Option<Vec<u8>>,
-    password: Option<Vec<u8>>,
-    nonce: Vec<u8>,
+    email: Option<String>,
+    username: Option<String>,
+    notes: Option<String>,
+    password: Option<String>,
+    nonce: String,
 }
+
 pub fn establish_connection() -> std::result::Result<rusqlite::Connection, rusqlite::Error> {
     Connection::open("./data.db")
 }
-pub fn create_table(connection: Connection) -> std::result::Result<usize, rusqlite::Error> {
+pub fn create_table(connection: &Connection) -> std::result::Result<usize, rusqlite::Error> {
     connection.execute(
         "create table if not exists password(
         id INTEGER NOT NULL PRIMARY KEY,
@@ -22,24 +23,59 @@ pub fn create_table(connection: Connection) -> std::result::Result<usize, rusqli
         email TEXT DEFAULT NULL,
         pass TEXT DEFAULT NULL,
         notes TEXT DEFAULT NULL,
-        aes_nonce TEXT NOT NULL
+        nonce TEXT NOT NULL
       );",
         (),
     )
 }
 pub fn get_password(
-    connection: Connection,
-    name: String,
-) -> std::result::Result<usize, rusqlite::Error> {
+    connection: &Connection,
+    column_name: &str,
+    search_term: &str,
+) -> Result<Option<Password>, rusqlite::Error> {
+    let mut statement =
+        connection.prepare(format!("select * from password where {} = ?", column_name).as_str())?;
+
+    statement
+        .query_row([search_term], |row| {
+            Ok(Password {
+                id: row.get(0)?,
+                name: row.get(1)?,
+                username: row.get(2)?,
+                email: row.get(3)?,
+                password: row.get(4)?,
+                notes: row.get(5)?,
+                nonce: row.get(6)?,
+            })
+        })
+        .optional()
+}
+pub fn insert_or_update(
+    connection: &Connection,
+    password_name: &str,
+    column_name: &str,
+    data: &str,
+) {
     let statement = connection
-        .prepare("select * from password where name = ?;")
-        .unwrap();
-    statement.query(name);
+        .execute("insert into password({}) values (?) on conflict({}) do update set {}=? ");
 }
 
 #[cfg(test)]
 mod tests {
     use rusqlite::Connection;
+
+    fn insert_test_data(connection: &Connection) -> std::result::Result<usize, rusqlite::Error> {
+        connection.execute(
+            "insert into password (name, username, email, pass, nonce) VALUES (?1, ?2, ?3, ?4, ?5)",
+            (
+                "test_name",
+                "cool_user1",
+                "cool_user@usermail.com",
+                "12345",
+                "nonce42",
+            ),
+        )
+    }
 
     #[test]
     fn establish_connection() {
@@ -47,8 +83,31 @@ mod tests {
     }
     #[test]
     fn create_table() {
+        assert!(super::create_table(&Connection::open_in_memory().unwrap()).is_ok());
+    }
+    #[test]
+    fn test_data() {
         let connection = Connection::open_in_memory().unwrap();
-        let result = super::create_table(connection);
-        assert!(result.is_ok());
+        super::create_table(&connection).unwrap();
+
+        // just make sure that the insert test data function is working and inserts a row
+        let insert_result = insert_test_data(&connection).unwrap();
+        assert_eq!(insert_result, 1);
+    }
+    #[test]
+    fn get_password() {
+        // basically just util functions
+        let connection = Connection::open_in_memory().unwrap();
+        super::create_table(&connection).unwrap();
+        insert_test_data(&connection).unwrap();
+
+        let term = "test_name";
+        // this is the important function that we want to test
+        let result = super::get_password(&connection, "name", term)
+            .unwrap()
+            .unwrap();
+        // check that the values are as we set them in the test function
+        assert_eq!(result.name, term);
+        assert!(result.notes.is_none());
     }
 }
