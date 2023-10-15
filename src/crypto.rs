a2cc1267274e32aeea1d27a3225a63cbe5cb7d8d
dvub <dvubdevs@gmail.com>
2023-10-15 01:35:22 -0700

    add insert crypto function

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,12 +1,21 @@
-use aes_gcm::aead::{
-    consts::{B0, B1},
-    generic_array::GenericArray,
+use aes_gcm::{
+    aead::{
+        consts::{B0, B1},
+        generic_array::GenericArray,
+        Aead, OsRng,
+    },
+    aes::Aes256,
+    AeadCore, Aes256Gcm, AesGcm, Key, KeyInit,
 };
 use pbkdf2::pbkdf2_hmac;
 use sha2::{
     digest::typenum::{UInt, UTerm},
     Digest, Sha256,
 };
+
+use crate::db_ops::GetPasswordError;
+
+pub type Nonce = GenericArray<u8, UInt<UInt<UInt<UInt<UTerm, B1>, B1>, B0>, B0>>;
 
 /// Hashes `text` using `Sha256`.
 pub fn hash(
@@ -27,4 +36,31 @@ pub fn derive_key(master_password: impl AsRef<[u8]>, kdf_salt: impl AsRef<[u8]>)
         &mut derived_key,
     );
     derived_key
+}
+
+pub fn decrypt_password_field(
+    data: &str,
+    decoded_nonce: &[u8],
+    cipher: &AesGcm<Aes256, UInt<UInt<UInt<UInt<UTerm, B1>, B1>, B0>, B0>>,
+) -> Result<String, GetPasswordError> {
+    let decoded = hex::decode(data)?;
+    let decrypted = cipher
+        .decrypt(GenericArray::from_slice(decoded_nonce), decoded.as_ref())
+        .map_err(GetPasswordError::AesGcm)?;
+    Ok(String::from_utf8(decrypted)?)
+}
+
+pub fn encrypt_password_field(
+    password_name: &str,
+    column_name: &str,
+    data: &str,
+    master: &str,
+    nonce: &Nonce,
+) -> Result<String, aes_gcm::Error> {
+    let derived_key = derive_key(master, password_name);
+    let key = Key::<Aes256Gcm>::from_slice(&derived_key);
+    let cipher = Aes256Gcm::new(key);
+
+    let encrypted = cipher.encrypt(nonce, data.as_bytes())?;
+    Ok(hex::encode(encrypted))
 }
