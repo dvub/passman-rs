572cc7f97348f7c190a4f8b84659992091b1b07c
dvub <dvubdevs@gmail.com>
2023-10-20 16:15:22 -0700

    add Display impl for enum

--- src/password.rs
+++ src/password.rs
@@ -1,8 +1,9 @@
+use core::fmt;
 use std::fmt::Display;
 
 #[derive(Debug)]
 /// A struct to represent a password
-pub struct Password {
+pub struct PasswordInfo {
     /// Password ID, auto-incremented by SQLite database. do not set this yourself!
     pub id: i32,
     /// The password name. Must be unique or will fail SQLite constraints.
@@ -16,11 +17,22 @@ pub struct Password {
     /// Optional password field.
     pub password: Option<String>,
 }
+#[derive(Debug)]
 pub enum PasswordField {
     Email,
     Username,
     Notes,
-    Password
+    Password,
 }
 
-impl Display for PasswordField
+impl Display for PasswordField {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let str = match self {
+            PasswordField::Email => "email",
+            PasswordField::Username => "username",
+            PasswordField::Password => "pass",
+            PasswordField::Notes => "notes",
+        };
+        write!(f, "{}", str)
+    }
+}
