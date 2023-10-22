04dc86d1d3027d1a0e4649d85dab85dc7648cab0
dvub <dvubdevs@gmail.com>
2023-10-22 01:15:25 -0700

    work on clap logic

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,16 @@ mod args;
 mod backend;
 mod cli;
 
-use args::PwdArgs;
-use backend::db_ops::util::{create_table, establish_connection};
+use args::{PasswordCommands, PasswordTypes, PwdArgs};
+use backend::{
+    crypto::generate_password,
+    db_ops::{
+        crud::insert_data,
+        util::{create_table, establish_connection},
+    },
+    error::BackendError,
+    password::PasswordField,
+};
 use clap::Parser;
 use cli::interactive;
 
@@ -24,6 +32,54 @@ fn main() -> anyhow::Result<()> {
     if args.interactive {
         interactive(&connection)?;
         return Ok(());
+    }
+    if args.command.is_none() {
+        println!("No command was supplied. Use -h or --help for more information.");
+        return Ok(());
+    }
+
+    let command = args.command.unwrap();
+    let master = args.master_password.unwrap();
+    match command {
+        PasswordCommands::Add {
+            name,
+            email,
+            username,
+            notes,
+            password_type,
+        } => {
+            let password = password_type.map(|t| match t {
+                PasswordTypes::Manual { password } => password,
+                PasswordTypes::Auto { length } => generate_password(length),
+            });
+            let fields = [email, username, notes, password]
+                .iter()
+                .enumerate()
+                .for_each(|(index, field)| {
+                    if field.is_some() {
+                        let column_name = match index {
+                            0 => PasswordField::Email,
+                            1 => PasswordField::Username,
+                            2 => PasswordField::Notes,
+                            3 => PasswordField::Password,
+                            _ => PasswordField::Email,
+                        };
+                        let data = field.unwrap();
+                        insert_data(&connection, &name, &master, column_name, &data);
+                    }
+                });
+        }
+        PasswordCommands::Get { name } => {}
+        PasswordCommands::Update {
+            name,
+            new_name,
+            email,
+            username,
+            notes,
+            password_type,
+        } => {}
+        PasswordCommands::List => {}
+        PasswordCommands::Delete { name, confirm } => {}
     }
 
     Ok(())
